01657e9207abb6a10bda0f81c77b65069523f4b0
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2019-06-17 22:10:42 +0100

    Added empty AuxiliaryState as an example

--- src/blockMonitor/blockchainMachine.ts
+++ src/blockMonitor/blockchainMachine.ts
@@ -3,11 +3,13 @@ import { BlockProcessor } from "./blockProcessor";
 import { IBlockStub } from "./blockStub";
 
 // Generic class to handle the anchor statee of a blockchain state machine
-export class BlockchainMachine<TState extends object> extends EventEmitter {
+export class BlockchainMachine<TAnchorState extends object, TAuxState extends object> extends EventEmitter {
     public static NEW_STATE_EVENT = "new_state";
 
-    private blockStates = new WeakMap<IBlockStub, TState>();
-    private mHeadState: TState | null = null;
+    private blockStates = new WeakMap<IBlockStub, TAnchorState>();
+    private mHeadState: TAnchorState | null = null;
+
+    private auxiliaryState: TAuxState;
 
     public get headState() {
         return this.mHeadState;
@@ -15,10 +17,17 @@ export class BlockchainMachine<TState extends object> extends EventEmitter {
 
     constructor(
         private blockProcessor: BlockProcessor,
-        private initialState: TState,
-        private reducer: (prevState: TState, block: IBlockStub) => TState
+        private initialAnchorState: TAnchorState,
+        initialAuxiliaryState: TAuxState,
+        private reducer: (
+            prevAnchorState: TAnchorState,
+            prevAuxState: TAuxState,
+            block: IBlockStub
+        ) => [TAnchorState, TAuxState]
     ) {
         super();
+
+        this.auxiliaryState = initialAuxiliaryState;
         this.processNewHead = this.processNewHead.bind(this);
 
         blockProcessor.on(BlockProcessor.NEW_HEAD_EVENT, this.processNewHead);
@@ -36,14 +45,14 @@ export class BlockchainMachine<TState extends object> extends EventEmitter {
 
         // start from the oldest, compute each block's state
         ancestorsToAdd.reverse();
-        let state: TState | null = null;
+        let state: TAnchorState | null = null;
         for (const block of ancestorsToAdd) {
             const parentBlock = this.blockProcessor.blockCache.getBlockStub(block.parentHash);
 
             // the previous state is the state of the parent block if available, or the initial state otherwise
-            const prevState = parentBlock ? this.blockStates.get(parentBlock)! : this.initialState;
+            const prevAnchorState = parentBlock ? this.blockStates.get(parentBlock)! : this.initialAnchorState;
 
-            state = this.reducer(prevState, block);
+            [state, this.auxiliaryState] = this.reducer(prevAnchorState, this.auxiliaryState, block);
             this.blockStates.set(block, state);
         }
 
@@ -51,7 +60,7 @@ export class BlockchainMachine<TState extends object> extends EventEmitter {
         this.mHeadState = state;
 
         // TODO: should we (deeply) compare old state and new state and only emit if different?
-        // Probably not, it might be expensive/inefficient depending on what is in TState
+        // Probably not, it might be expensive/inefficient depending on what is in TAnchorState
         this.emit(BlockchainMachine.NEW_STATE_EVENT, oldState, state);
     }
 }
